ed430845a9925b8bc7b57423449fc3e7b19cb6cd
Hassan <libanon-hc1@hotmail.de>
2023-12-16 19:28:31 +0200

    rt

--- database/user.model.ts
+++ database/user.model.ts
@@ -14,7 +14,7 @@ const UserSchema = new Schema({
     clerkId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String,  unique: true },
     password: { type: String },
     picture: { type: String, required: true },
     joinedAt: { type: Date, default: Date.now},
